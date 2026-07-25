b60ebc1833d41c7b8f741c4174fae6b50279df70
CodeGreedy <codegreedy79@gmail.com>
2026-07-25 18:53:24 +0530

    Implement Insertion Sort in Insertion_Sort.cpp

---
+++ Problems/Sorting/Inserting Sort/Insertion_Sort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(vector <int> arr, int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+int main()
+{
+    int vector<int> arr = {7, 5, 3, 9, 2};
+    int n = arr.size():
+
+    insertionSort(arr, n);
+
+    for (int x : arr)
+        cout << x << " ";
+
+    return 0;
+}
