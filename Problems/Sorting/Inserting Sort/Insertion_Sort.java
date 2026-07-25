8ab21423a8f3e44c9e5d6ce109b9fd33b60fba52
CodeGreedy <codegreedy79@gmail.com>
2026-07-25 18:54:00 +0530

    Add InsertionSort class with sorting logic
    Implement Insertion Sort algorithm in Java.

---
+++ Problems/Sorting/Inserting Sort/Insertion_Sort.java
@@ -0,0 +1,30 @@
+public class InsertionSort {
+
+    static void insertionSort(int[] arr) {
+
+        for (int i = 1; i < arr.length; i++) {
+
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {7, 5, 3, 9, 2};
+
+        insertionSort(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
