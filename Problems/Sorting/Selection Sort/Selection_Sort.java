bc406e94b7c4db8a7d8ff555a293f5ddb0c93684
CodeGreedy <codegreedy79@gmail.com>
2026-07-25 18:56:45 +0530

    Refactor selection sort by adding swap method in Java

---
+++ Problems/Sorting/Selection Sort/Selection_Sort.java
@@ -0,0 +1,36 @@
+public class SelectionSort {
+    static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    static void selectionSort(int[] arr) {
+
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            swap(arr, i, minIndex);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {64, 25, 12, 22, 11};
+
+        selectionSort(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
